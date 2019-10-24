f9e10543043edb47e8977deeb5e5c3e9baf3f75c
raul0800 <raul0800@mail.ru>
2019-10-24 07:46:10 +0300

    new version

--- build.sbt
+++ build.sbt
@@ -2,4 +2,25 @@ name := "ex"
 
 version := "0.1"
 
-scalaVersion := "2.13.1"
+scalaVersion := "2.12.9"
+
+val Http4sVersion = "0.20.8"
+val CirceVersion = "0.11.1"
+
+//libraryDependencies ++= Seq(
+//  "org.http4s"      %% "http4s-blaze-server" % Http4sVersion,
+//  "org.http4s"      %% "http4s-blaze-client" % Http4sVersion,
+//  "org.http4s"      %% "http4s-circe"        % Http4sVersion,
+//  "org.http4s"      %% "http4s-dsl"          % Http4sVersion,
+//  "io.circe"        %% "circe-generic"       % CirceVersion,
+//  "org.http4s"      %% "http4s-circe"        % Http4sVersion,
+//  "io.circe"        %% "circe-generic"       % CirceVersion,
+//  "io.circe"        %% "circe-literal"       % CirceVersion,
+//  "org.json4s"      %% "json4s-jackson"      % "3.2.11",
+//  "org.http4s"      %% "http4s-json4s-jackson" % Http4sVersion,
+//  "org.json4s"      %% "json4s-native"        % "3.2.11")
+libraryDependencies += "net.liftweb" % "lift-sbt" % "2.4"
+libraryDependencies += "net.liftweb" %% "lift-json" % "3.1.0-M1"
+libraryDependencies += "org.typelevel" %% "cats-core" % "2.0.0"
+
+scalacOptions += "-Ypartial-unification"
---
+++ src/main/scala/com/sol1/main.scala
@@ -0,0 +1,72 @@
+package com.sol1
+
+import net.liftweb.json.DefaultFormats
+import net.liftweb.json._
+import scala.math._
+//import cats._
+//import cats.implicits._
+
+object main extends App {
+  implicit val formats = DefaultFormats
+
+  val source = scala.io.Source.fromFile("inp.txt")
+
+  //println(readJson)
+  //println(toLine())
+  //  for {
+  //    lst <- readJson
+  //    _ <- toLine(lst)
+  //  }
+  val x = readJson
+  val y = circuitList(x)
+  val z = toLine(y)
+  println(z)
+  calculateAngle(z)
+
+  //  for {
+  //    lst <- readJson
+  //    circLst <- circuitList(lst)
+  //  }
+
+  def readJson: List[(Double, Double)] = {
+    val lines: String = try source.mkString finally source.close()
+    //    val json = parse(lines)
+    //    val elements = (json).children
+    //    val m = elements map(x => x.extract[List[List[Double]]])
+    //println(m flatMap (x => x flatMap (y => List((y(0), y(1))))))
+    parse(lines).
+      children.
+      map(x => x.extract[List[List[Double]]]).
+      flatMap(y =>
+        y flatMap (z =>
+          List((z(0), z(1)))))
+    //m flatMap (x => x flatMap (y => List((y(0), y(1)))))
+  }
+
+  def circuitList(list: List[(Double, Double)]) = {
+    list :+ (list.head)
+  }
+
+  def toLine(list: List[(Double, Double)]) = {
+    list.sliding(2).toList
+  }
+
+  //  double get_ang (const pt & a, const pt & b) {
+  //    double ang = abs (atan2 (a.y, a.x) - atan2 (b.y, b.x));
+  //    return min (ang, 2*PI-ang);
+  //  }
+  def calculateAngle(list: List[List[(Double, Double)]]) = {
+    val cortege = list.map(x =>
+      (x(0),
+        x(1),
+        sqrt(pow((x(0)._1 - x(1)._1), 2) + pow((x(0)._2 - x(1)._2), 2))
+      ))
+
+    println(cortege)
+  }
+
+  //  for {
+  //    lst <- readJson
+  //    rad <-
+  //  } yield rad
+}
