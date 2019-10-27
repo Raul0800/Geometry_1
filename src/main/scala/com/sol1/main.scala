00d5b1811c7f10d254ca5257a1c3afbd887d3061
raul0800 <raul0800@mail.ru>
2019-10-27 23:18:55 +0300

    new methods, added some new logic

--- src/main/scala/com/sol1/main.scala
+++ src/main/scala/com/sol1/main.scala
@@ -20,31 +20,24 @@ object main extends App {
   //val x = readJson
   //val y = circuitList(x)
   //val z = toLine(y)
-//  println(z)
-//  calculateAngle(z)
+  //  println(z)
+  //  calculateAngle(z)
 
-  val lines = for{
-    lst <- readJson
-    circLst <- circuitList(lst)
-    lines <- toLine(circLst)
-  } yield (lines)
+  mainLoop()
 
-  print(lines)
-
-
-
-
-  //  for {
-  //    lst <- readJson
-  //    circLst <- circuitList(lst)
-  //  }
-
-  def readJson: Either[String, List[(Double, Double)]] = {
+  def readData: Either[String, List[(Double, Double)]] = {
     val lines: String = try source.mkString finally source.close()
     //    val json = parse(lines)
     //    val elements = (json).children
     //    val m = elements map(x => x.extract[List[List[Double]]])
     //println(m flatMap (x => x flatMap (y => List((y(0), y(1))))))
+    println(parse(lines).
+      children.
+      map(x => x.extract[List[List[Double]]]).
+      flatMap(y =>
+        y flatMap (z =>
+          List((z(0), z(1))))
+      ))
     Right(parse(lines).
       children.
       map(x => x.extract[List[List[Double]]]).
@@ -55,30 +48,78 @@ object main extends App {
     //m flatMap (x => x flatMap (y => List((y(0), y(1)))))
   }
 
-  def circuitList(list: List[(Double, Double)]):Either[String, List[(Double, Double)]] = {
+  def circuitList(list: List[(Double, Double)]): Either[String, List[(Double, Double)]] = {
+    println(list :+ (list.head))
     Right(list :+ (list.head))
   }
 
   def toLine(list: List[(Double, Double)]): Either[String, List[List[(Double, Double)]]] = {
+    println(list.sliding(2).toList)
     Right(list.sliding(2).toList)
   }
 
-  //  double get_ang (const pt & a, const pt & b) {
-  //    double ang = abs (atan2 (a.y, a.x) - atan2 (b.y, b.x));
-  //    return min (ang, 2*PI-ang);
-  //  }
-  def calculateAngle(list: List[List[(Double, Double)]]) = {
+  def calculateLenght(list: List[List[(Double, Double)]]): Either[String,
+    List[((Double, Double), (Double, Double), Double)]] = {
     val cortege = list.map(x =>
       (x(0),
         x(1),
         sqrt(pow((x(0)._1 - x(1)._1), 2) + pow((x(0)._2 - x(1)._2), 2))
       ))
-
     println(cortege)
+    Right(cortege)
   }
 
-  //  for {
-  //    lst <- readJson
-  //    rad <-
-  //  } yield rad
+  def calculateAngle(list: List[((Double, Double), (Double, Double), Double)]):
+  Either[String, (List[((Double, Double), (Double, Double), Double)], List[Double])] = {
+    val angels = (list :+ list.head).sliding(2).toList.map(x =>
+      (x(0), x(1)) match {
+        case (a, b) => {
+          val c = sqrt(pow(a._1._1 - b._2._1, 2) + pow(a._1._2 - b._2._2, 2))
+          acos((pow(a._3, 2) + pow(b._3, 2) - pow(c, 2))
+            / (2 * a._3 * b._3))
+        }
+      })
+    println(list /* :+ list.head*/ , angels)
+    Right(list /* :+ list.head*/ , angels)
+  }
+
+  def calculateHigth(pair: (List[((Double, Double), (Double, Double), Double)], List[Double])):
+  Either[String, (List[((Double, Double), (Double, Double), Double)], List[Double], List[Double])] = {
+    Right(pair._1, pair._2, pair._1.map(x => {
+      val iter1 = pair._1.indexOf(x)
+      val iter2 = if (iter1 == 0) pair._2.size - 1 else if (iter1 == pair._2.size - 1) 0 else iter1 + 1
+
+      (x._3, pair._2(iter1), pair._2(iter2)) match {
+        case (l, alph, bet) => {
+          println(l, alph, bet)
+          l * (sin(alph / 2) * sin(bet / 2)) / sin(alph / 2 + bet / 2)
+        }
+      }
+    })
+    )
+  }
+
+  def mainLoop():
+  Either[String, (List[((Double, Double), (Double, Double), Double)], List[Double], List[Double])] = {
+    val tuple3 = for {
+      lst <- readData
+      circLst <- circuitList(lst)
+      lines <- toLine(circLst)
+      cortege <- calculateLenght(lines)
+      pair <- calculateAngle(cortege)
+      tuple3 <- calculateHigth(pair)
+    } yield (tuple3)
+
+    println(tuple3)
+
+    val rTuple3 = tuple3.right.get
+
+    //
+    //some logic
+    //
+    if (rTuple3._3.size > 2)
+      mainLoop()
+    else
+      tuple3
+  }
 }
